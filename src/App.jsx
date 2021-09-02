c9603a8cdd68289680ecf6627ef0d91dd789e28d
Abdul Latif Mubasir <bj.angel119@gmail.com>
2021-09-02 10:48:11 +0700

    chore: setup axios and create action for get auctions from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React , { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { fetchAuction } from './redux/actions/auctions'
+import axios from './axios'
 
 const App = () => {
+    const dispatch = useDispatch()
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                let auctions = await axios.get('/auctions')
+                auctions = auctions.data
+                dispatch(fetchAuction(auctions))
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData()
+    }, [])
     return (
         <div className="flex justify-center items-center h-screen">
             <h1 className="text-center text-6xl text-blue-400 font-bold ">Bid Donation Project</h1>
--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const instance = axios.create({
 	baseURL: process.env.REACT_APP_API,
-	withCredentials: true,
 })
 
 export default instance;
