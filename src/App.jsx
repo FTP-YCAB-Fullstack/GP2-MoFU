8db809c43cbf255cfa701332d2e16189a8db5994
Abdul Latif Mubasir <bj.angel119@gmail.com>
2021-09-02 08:02:01 +0700

    chore: setup react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="text-center text-6xl text-blue-400 font-bold ">Bid Donation Project</h1>
+        </div>
+    )
+}
+
+export default App
