85dee247da23607aac19a11db86864b04100904b
Abdul Latif Mubasir <bj.angel119@gmail.com>
2021-09-04 08:48:14 +0700

    chore: update redux/reducers/store.js

--- src/redux/reducers/store.js
+++ src/redux/reducers/store.js
@@ -2,12 +2,18 @@ import { combineReducers, createStore , applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import auctionReducer from "./auctionsReducer";
 import { composeWithDevTools } from 'redux-devtools-extension'
+import modalReducer from "./modalReducer";
+import usersReducer from "./usersReducer";
+import bidReducer from "./bidReducer";
 
 const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
 
 const store = createStore(
     combineReducers({
-        auctions: auctionReducer
+        auctions: auctionReducer,
+        modal: modalReducer,
+        users: usersReducer,
+        bids: bidReducer,
     }),
     composedEnhancer
 )
