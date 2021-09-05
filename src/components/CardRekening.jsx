4b9af67740e39790d0ff19958058f6abae49cbb4
Yusril Ihza <yusrilihzaprasetya@gmail.com>
2021-09-05 13:36:35 +0700

    chore : create CardRekening.jsx

---
+++ src/components/CardRekening.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const CardRekening = () => {
+  return (
+    <div className="w-1/4 py-10 px-10">
+      <div className="my-8 rounded-lg w-full bg-red-300 p-8 mx-auto">
+        <div className="text-white">
+          <h4 className="text-center text-xl font-bold">Metode Pemabayaran</h4>
+          <p>Admin</p>
+          <p>Bank BRC</p>
+          <p>8098 9821 2131 322</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardRekening;
