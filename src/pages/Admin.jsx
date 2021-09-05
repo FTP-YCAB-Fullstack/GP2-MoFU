6cd1b4175638d3ccb15e3fc28f08e85a44e5093a
Dmaul0906 <dimasmaulana0906@gmail.com>
2021-09-05 13:39:50 +0700

    chore: create Admin.jsx

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import CollectTotal from "../components/CollectTotal"
+
+const Admin = () => {
+    return (
+        <div>
+            <h1> Ini halaman admin </h1>
+            <CollectTotal />
+        </div>
+    )
+}
+
+export default Admin
