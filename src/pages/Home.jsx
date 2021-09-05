df0d5a98456b33b3a7ed33143bf30654cd511fb8
Dmaul0906 <dimasmaulana0906@gmail.com>
2021-09-05 13:37:25 +0700

    chore :  update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,22 @@ import CardLelang from '../components/CardLelang'
 import FormBarang from '../components/FormBarang'
 import Modal from '../components/Modal'
 import Content from '../components/Content'
+import CovidCase from '../components/CovidCase'
+import CardRekening from '../components/CardRekening'
+import { useSelector } from 'react-redux'
 
 const Home = () => {
+    const auth = useSelector(state => state.auth )
     return (
-        <div className="pb-20">
+        <div className="pb-20 flex">
+            <CovidCase />
             <Content>
-                <FormBarang />
+                { 
+                    auth.status ? <FormBarang  /> : ""
+                }
                 <CardLelang />
             </Content>
+            <CardRekening />
             <Modal />
         </div>
     )
