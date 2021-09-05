7f255b3278bc06e3a396e980e79f6eb7448333ef
Dedyyoga <08776215637.ky@gmail.com>
2021-09-05 14:34:17 +0800

    feat: update content style

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Content = ({children}) => {
     return (
-        <div className="flex flex-col w-1/2 mx-auto py-10">
+        <div className="flex flex-col w-1/2 py-10">
             {children}
         </div>
     )
