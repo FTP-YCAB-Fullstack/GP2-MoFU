4caf5e8d0391c62d262540305c7e813c78be5c79
Abdul Latif Mubasir <bj.angel119@gmail.com>
2021-09-05 13:02:38 +0700

    chore : update redux/reducers/usersReducer.js

--- src/redux/reducers/usersReducer.js
+++ src/redux/reducers/usersReducer.js
@@ -1,11 +1,12 @@
 const usersReducer = (state = [], action) => {
   switch (action.type) {
-    case "FETCH_USERS":
-      return action.payload.data;
-
-    default:
-      return state;
+      case "FETCH_USERS":
+          return action.payload.data
+      case "REGISTER_USER":
+          return [...state , action.payload.data]
+      default:
+          return state
   }
-};
+}
 
-export default usersReducer;
+export default usersReducer
