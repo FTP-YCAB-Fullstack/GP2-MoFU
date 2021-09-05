cb0775a9b3e9b42bc4117a14df1345ce442b8e5a
Abdul Latif Mubasir <bj.angel119@gmail.com>
2021-09-05 13:58:27 +0700

    feat : update App.jsx and Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React , { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { fetchAuction } from './redux/actions/auctions'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchAuctions } from './redux/actions/auctions'
+import { fetchUsers } from './redux/actions/users'
+import { fetchBids } from './redux/actions/bids'
+import { positive,recovery,death } from './redux/actions/covid' 
+
+ 
 import axios from './axios'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch , Redirect } from 'react-router-dom'
 import Home from './pages/Home'
+import Admin from './pages/Admin'
 import Navbar from './components/Navbar'
+
+const PrivateRoute  = (props) => {
+    const auth = useSelector(state => state.auth)
+    return auth.status ? 
+        <Route {...props} >{props.children}</Route>
+    :
+        <Redirect to="/" />
+}
 
 const App = () => {
     const dispatch = useDispatch()
@@ -14,18 +28,36 @@ const App = () => {
             try {
                 let auctions = await axios.get('/auctions')
                 auctions = auctions.data
-                dispatch(fetchAuction(auctions))
+                dispatch(fetchAuctions(auctions))
+                let users = await axios.get(`/users`)
+                users = users.data
+                dispatch(fetchUsers(users))
+                let bids = await axios.get('/bids')
+                bids = bids.data
+                dispatch(fetchBids(bids))
+
+                // fetch covid
+                // https://apicovid19indonesia-v2.vercel.app/api/indonesia
+                let dataCovid = await axios.get('https://apicovid19indonesia-v2.vercel.app/api/indonesia')
+                dataCovid = dataCovid.data
+                dispatch(positive(dataCovid.positif))
+                dispatch(recovery(dataCovid.sembuh))
+                dispatch(death(dataCovid.meninggal))
+
+                
             } catch (error) {
                 console.log(error)
             }
         }
         fetchData()
     }, [])
+
     return (
         <>
             <Navbar />
             <Switch>
                 <Route exact path="/" component={Home} />
+                <PrivateRoute path="/admin" component={Admin} /> 
             </Switch>
         </>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch,useSelector } from 'react-redux'
 import { modal } from '../redux/actions/modal'
+import { logoutUser } from "../redux/actions/auth"
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     const dispatch = useDispatch()
+    const auth = useSelector(state => state.auth )
+
     return (
         <div>
             <nav className="flex justify-between bg-indigo-500 p-3">
-                <div className="left py-2 mx-4 text-white font-bold text-2xl">Logo</div>
+                <Link to="/"  className="left py-2 mx-4 text-white font-bold text-2xl">Logo</Link>
                 <div className="right mx-4">
-                    <button onClick={() => dispatch(modal("login"))} className="text-xl px-3 bg-white rounded-lg mx-2 py-2 font-bold">Login</button>
-                    <button onClick={() => dispatch(modal("register"))} className="text-xl px-3 bg-white rounded-lg mx-2 py-2 font-bold">Register</button>
+                    {
+                        !auth.status ? 
+                        <>
+                            <button onClick={() => dispatch(modal("login"))} className="text-xl px-3 bg-white rounded-lg mx-2 py-2 font-bold">Login</button>
+                            <button onClick={() => dispatch(modal("register"))} className="text-xl px-3 bg-white rounded-lg mx-2 py-2 font-bold">Register</button>
+                        </>
+                        :
+                        <>
+                            {
+                                auth.user.role_id === 1 ? 
+                                    <Link to="/admin" className="text-xl px-3 bg-white rounded-lg mx-2 py-2 font-bold">Admin</Link>
+                                    :
+                                    ""
+                            }
+                            <button onClick={() => dispatch(logoutUser())} className="text-xl px-3 bg-white rounded-lg mx-2 py-2 font-bold">Log-out</button>
+                        </>
+                    }
                 </div>
             </nav>
         </div>
