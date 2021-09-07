206f9e98a4f3ac6106264db946edb2fa501eeaa2
Abdul Latif Mubasir <bj.angel119@gmail.com>
2021-09-07 14:11:26 +0700

    chore : upload asset

--- src/components/CardRekening.jsx
+++ src/components/CardRekening.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React from "react"
+import poster from "../assets/poster_right.jpg"
+import logoWarna from "../assets/logo_tulisan_warna.svg"
+import { useHistory } from "react-router"
+import { motion } from "framer-motion"
 
 const CardRekening = () => {
-  return (
-    <div className="w-1/4 py-10 px-10">
-      <div className="my-8 rounded-lg w-full bg-red-300 p-8 mx-auto">
-        <div className="text-white">
-          <h4 className="text-center text-xl font-bold">Metode Pemabayaran</h4>
-          <p>Admin</p>
-          <p>Bank BRC</p>
-          <p>8098 9821 2131 322</p>
+    const history = useHistory()
+    return (
+        <div className={(history.location.pathname.includes('payment') ? "w-full " : "hidden md:block ") + " md:w-1/4 px-4 md:px-10"}>    
+            <motion.div
+            initial={{ opacity: 0 , x: 50 }}
+            animate={{ opacity: 1 , x: 0 }}
+            transition={{ duration: 1.4 , delay: 0.3, type: 'spring' , bounce: 0.7 }}
+            className=" mb-4 rounded-lg w-full bg-white shadow-md p-8 mx-auto">
+                <img src={logoWarna} alt="logo warna" />
+            </motion.div>
+            <motion.div
+            initial={{ opacity: 0 , x: 50 }}
+            animate={{ opacity: 1 , x: 0 }}
+            transition={{ duration: 1.4 ,delay: 0.6, type: 'spring' , bounce: 0.7 }}
+            className="overflow-hidden rounded-lg shadow-md">
+                <img src={poster} alt="poster" />
+            </motion.div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default CardRekening;
+export default CardRekening
