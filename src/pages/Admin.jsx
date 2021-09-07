e2c15989dabe5c6dec78f7d46870896e19951fc3
Dmaul0906 <dimasmaulana0906@gmail.com>
2021-09-07 13:24:50 +0700

    style: update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,13 +1,17 @@
-import React from "react"
-import CollectTotal from "../components/CollectTotal"
+import React from "react";
+import CollectTotal from "../components/CollectTotal";
 
 const Admin = () => {
-    return (
-        <div>
-            <h1> Ini halaman admin </h1>
-            <CollectTotal />
+  return (
+    <div className="bg-gray-100 h-full pt-4 pb-72">
+      <div className="my-5">
+        <h1 className="text-center text-3xl font-bold"> Dashboard Admin </h1>
+        <div className="flex justify-center text-xl ">
+          <CollectTotal />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Admin
+export default Admin;
