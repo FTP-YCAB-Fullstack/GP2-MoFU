67da94a9003814e541c6f24be3c5a20cb8ab3427
Dmaul0906 <dimasmaulana0906@gmail.com>
2021-09-07 13:34:39 +0700

    feat: update Covid.jsx

--- src/pages/Covid.jsx
+++ src/pages/Covid.jsx
@@ -1,25 +1,12 @@
-import { combineReducers, createStore, applyMiddleware } from "redux";
-import thunkMiddleware from "redux-thunk";
-import auctionReducer from "./auctionsReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
-import modalReducer from "./modalReducer";
-import usersReducer from "./usersReducer";
-import bidReducer from "./bidReducer";
-import authReducer from "./authReducer";
-import covidReducer from "./covidReducer";
+import React from "react";
+import CovidCase from "../components/CovidCase";
 
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+const Covid = () => {
+  return (
+    <div className="bg-gray-100">
+      <CovidCase />
+    </div>
+  );
+};
 
-const store = createStore(
-  combineReducers({
-    auctions: auctionReducer,
-    modal: modalReducer,
-    users: usersReducer,
-    bids: bidReducer,
-    auth: authReducer,
-    covid: covidReducer,
-  }),
-  composedEnhancer
-);
-
-export default store;
+export default Covid;
